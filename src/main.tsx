f6ddd4da9c0ac2d53768d43c0a2541292919004d
oyhoyhk <oyhoyhk@naver.com>
2023-05-25 11:44:28 +0900

    vercel analyze 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 import "./index.css";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
